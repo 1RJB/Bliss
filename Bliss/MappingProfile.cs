7aa668b382b1a7fd0cd4666c613e9f8412283921
Ryan <ryanb0717@gmail.com>
2025-01-13 10:48:39 +0800

    Add project files.

---
+++ Bliss/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Bliss.Models;
+
+namespace Bliss
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Tutorial, TutorialDTO>();
+            CreateMap<User, UserDTO>();
+            CreateMap<User, UserBasicDTO>();
+        }
+    }
+}
