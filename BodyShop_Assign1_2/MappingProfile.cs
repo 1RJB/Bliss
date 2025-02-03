8f832f9fd09cf72189a77488ef3093207891d29d
Elizabeth Chan <elizabethchan2583@gmail.com>
2025-02-03 13:57:20 +0800

    VS22-Update 3

---
+++ BodyShop_Assign1_2/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BodyShop_Assign1_2.Models;
+
+namespace BodyShop_Assign1_2
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, TutorialDTO>();
+            CreateMap<User, UserBasicDTO>();
+        }
+
+    }
+}
---
+++ BodyShop_Assign1_2/Models/Cart.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BodyShop_Assign1_2.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+       
+        [Required]
+        public decimal TotalPrice { get; set; }
+
+        [Required]
+        public int TotalProducts { get; set; }
+
+        
+
+        // Foreign key for the User
+        public int UserId { get; set; }
+
+        // Navigation property to represent the many-to-one relationship
+        public User? User { get; set; }
+    }
+}
