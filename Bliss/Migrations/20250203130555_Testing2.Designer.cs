b655881051020ef68242fd2f300c69b3e3bb70a2
Elizabeth Chan <elizabethchan2583@gmail.com>
2025-02-03 21:07:58 +0800

    testing2

--- Bliss/Migrations/20250203130555_Testing2.Designer.cs
+++ Bliss/Migrations/20250203130555_Testing2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bliss;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bliss.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250203130555_Testing2")]
+    partial class Testing2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
