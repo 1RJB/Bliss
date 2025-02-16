9192336987858dc96158a7f00101914107c65ff1
Ryan <ryanb0717@gmail.com>
2025-02-17 03:43:34 +0800

    Add Verify2FARequest model for 2FA verification

---
+++ Bliss/Models/Verify2FARequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bliss.Models
+{
+    public class Verify2FARequest
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+    }
+}
