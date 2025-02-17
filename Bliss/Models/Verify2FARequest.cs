9146ed303a34bee747ad6a0a66f66aff67a50a5a
Ryan <ryanb0717@gmail.com>
2025-02-17 10:22:05 +0800

    Remove Email property and Required attribute from Code
    The `Verify2FARequest` class in the `Bliss.Models` namespace has
    been updated. The `Email` property, which was previously required,
    has been removed. Additionally, the `Required` attribute for the
    `Code` property has been removed, though the `Code` property
    itself remains with its `StringLength` attribute specifying a
    length of 6 characters.

--- Bliss/Models/Verify2FARequest.cs
+++ Bliss/Models/Verify2FARequest.cs
@@ -5,9 +5,6 @@ namespace Bliss.Models
     public class Verify2FARequest
     {
         [Required]
-        public string Email { get; set; }
-
-        [Required]
         [StringLength(6, MinimumLength = 6)]
         public string Code { get; set; }
     }
