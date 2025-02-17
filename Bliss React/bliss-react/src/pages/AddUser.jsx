6cd9196442ee283f4d3fa4f2d46ec635f240feb0
Ryan <ryanb0717@gmail.com>
2025-02-17 12:16:36 +0800

    Implement user addition form with validation and improved UI

--- Bliss React/bliss-react/src/pages/AddUser.jsx
+++ Bliss React/bliss-react/src/pages/AddUser.jsx
@@ -1,60 +1,141 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, Typography, TextField, Button, Paper, MenuItem } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import http from '../http';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddUser = () => {
+    const navigate = useNavigate();
     const [user, setUser] = useState({
         name: '',
         email: '',
         role: 'client', // default role
     });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setUser({
-            ...user,
-            [name]: value,
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Add your form submission logic here
-        console.log('User submitted:', user);
-    };
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+            role: 'client',
+        },
+        validationSchema: yup.object({
+            name: yup.string().trim()
+                .min(3, 'Name must be at least 3 characters')
+                .max(50, 'Name must be at most 50 characters')
+                .required('Name is required')
+                .matches(/^[a-zA-Z '-,.]+$/,
+                    "Name only allows letters, spaces and characters: ' - , ."),
+            email: yup.string().trim()
+                .email('Enter a valid email')
+                .max(50, 'Email must be at most 50 characters')
+                .required('Email is required'),
+            password: yup.string().trim()
+                .min(8, 'Password must be at least 8 characters')
+                .max(50, 'Password must be at most 50 characters')
+                .required('Password is required')
+                .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/,
+                    "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 digit, and 1 special character"),
+            confirmPassword: yup.string().trim()
+                .required('Confirm password is required')
+                .oneOf([yup.ref('password')], 'Passwords must match'),
+            role: yup.string().required('Role is required'),
+        }),
+        onSubmit: (data) => {
+            http.post("/user/add-user", data)
+                .then((res) => {
+                    toast.success('User added successfully!');
+                    setTimeout(() => { navigate("/users"); }, 3000);
+                })
+                .catch(function (err) {
+                    toast.error(`${err.response.data.message}`);
+                });
+        }
+    });
 
     return (
-        <div>
-            <h2>Add User</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Name:</label>
-                    <input
-                        type="text"
+        <Box sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }}>
+            <Paper elevation={3} sx={{ padding: 4, maxWidth: 500, width: '100%' }}>
+                <Typography variant="h5" sx={{ my: 2, textAlign: 'center' }}>
+                    Add User
+                </Typography>
+                <Box component="form" sx={{ mt: 1 }}
+                    onSubmit={formik.handleSubmit}>
+                    <TextField
+                        fullWidth margin="dense" autoComplete="off"
+                        label="Name"
                         name="name"
-                        value={user.name}
-                        onChange={handleChange}
-                        required
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.name && Boolean(formik.errors.name)}
+                        helperText={formik.touched.name && formik.errors.name}
                     />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
+                    <TextField
+                        fullWidth margin="dense" autoComplete="off"
+                        label="Email"
                         name="email"
-                        value={user.email}
-                        onChange={handleChange}
-                        required
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
                     />
-                </div>
-                <div>
-                    <label>Role:</label>
-                    <select name="role" value={user.role} onChange={handleChange}>
-                        <option value="client">Client</option>
-                        <option value="staff">Staff</option>
-                    </select>
-                </div>
-                <button type="submit">Add User</button>
-            </form>
-        </div>
+                    <TextField
+                        fullWidth margin="dense" autoComplete="off"
+                        label="Password"
+                        name="password" type="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+                    />
+                    <TextField
+                        fullWidth margin="dense" autoComplete="off"
+                        label="Confirm Password"
+                        name="confirmPassword" type="password"
+                        value={formik.values.confirmPassword}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+                        helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+                    />
+                    <TextField
+                        fullWidth margin="dense" autoComplete="off"
+                        label="Role"
+                        name="role"
+                        select
+                        value={formik.values.role}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.role && Boolean(formik.errors.role)}
+                        helperText={formik.touched.role && formik.errors.role}
+                    >
+                        <MenuItem value="client">Client</MenuItem>
+                        <MenuItem value="staff">Staff</MenuItem>
+                    </TextField>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        type="submit"
+                        sx={{ mt: 4, mb: 2 }}
+                    >
+                        Add User
+                    </Button>
+                </Box>
+            </Paper>
+            <ToastContainer />
+        </Box>
     );
 };
 
