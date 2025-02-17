426e0a960ee0b18d4120bd777895f25abd5071ab
Ryan <ryanb0717@gmail.com>
2025-02-17 09:30:17 +0800

    Add Requires2FASetup property to LoginResponse class
    The LoginResponse class has been updated to include a new boolean
    property named Requires2FASetup. This property indicates whether
    the user needs to set up two-factor authentication (2FA) as part
    of the login process.

--- Bliss/Models/LoginResponse.cs
+++ Bliss/Models/LoginResponse.cs
@@ -5,5 +5,8 @@
         public UserDTO User { get; set; } = new UserDTO();
 
         public string AccessToken { get; set; } = string.Empty;
+        
+        public bool Requires2FASetup { get; set; }
+
     }
 }
