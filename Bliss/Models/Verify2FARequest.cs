caa9d03b65015ec9239e53ee184c2b4459e51216
Ryan <ryanb0717@gmail.com>
2025-02-17 09:30:27 +0800

    Add validation to 2FA code length in Verify2FARequest
    Added [StringLength(6, MinimumLength = 6)] attribute to the Code
    property in the Verify2FARequest class to enforce a 6-character
    length constraint for the two-factor authentication code.

--- Bliss/Models/Verify2FARequest.cs
+++ Bliss/Models/Verify2FARequest.cs
@@ -8,6 +8,7 @@ namespace Bliss.Models
         public string Email { get; set; }
 
         [Required]
+        [StringLength(6, MinimumLength = 6)]
         public string Code { get; set; }
     }
 }
